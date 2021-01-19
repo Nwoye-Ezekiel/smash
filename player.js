d105f13a937ddbfe443e01fdf434eb55ceefa841
Nwoye-Ezekiel <57937355+Nwoye-Ezekiel@users.noreply.github.com>
2021-01-19 10:28:10 +0100

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,9 @@
 if (localStorage.name_activated == undefined) {
   localStorage.player_name = "Your Name";
 }
+$(window).load(function () {
+  $(".se-pre-con").fadeOut("slow");
+})
 document.getElementById("user").value = localStorage.player_name;
 function set_name(x) {
   localStorage.player_name = x.value;
