5ad169c4106d16c7bc3491f12c575260153b1f46
Nwoye-Ezekiel <57937355+Nwoye-Ezekiel@users.noreply.github.com>
2021-01-19 05:13:46 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+setTimeout(function () {
+  document.getElementById("name").style.animation = "random1 5s ease-in-out";
+}, 1);
+
+setTimeout(function () {
+  document.getElementById("glass").style.display = "block";
+}, 3000);
+
+setTimeout(function () {
+  document.getElementById("ball-container").style.display = "block";
+}, 3360);
+setTimeout(function () {
+  window.location.href = "home.html";
+}, 5500);
