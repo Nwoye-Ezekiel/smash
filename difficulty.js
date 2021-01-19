e590fde333b884d15bbb80d40d53fece908aabe8
Nwoye-Ezekiel <57937355+Nwoye-Ezekiel@users.noreply.github.com>
2021-01-19 10:26:28 +0100

    Update difficulty.js

--- difficulty.js
+++ difficulty.js
@@ -5,6 +5,9 @@ if (localStorage.difficulty == "Easy") {
 } else if (localStorage.difficulty == "Hard") {
   document.getElementById(localStorage.difficulty).checked = true;
 }
+$(window).load(function () {
+  $(".se-pre-con").fadeOut("slow");
+})
 function difficulty(d) {
   if (d == "Easy") {
     localStorage.difficulty = "Easy";
