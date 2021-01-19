9dddbde20af43db757ded03670253369905158d3
Nwoye-Ezekiel <57937355+Nwoye-Ezekiel@users.noreply.github.com>
2021-01-19 05:19:10 +0100

    Create difficulty.js

---
+++ difficulty.js
@@ -0,0 +1,19 @@
+if (localStorage.difficulty == "Easy") {
+  document.getElementById(localStorage.difficulty).checked = true;
+} else if (localStorage.difficulty == "Medium") {
+  document.getElementById(localStorage.difficulty).checked = true;
+} else if (localStorage.difficulty == "Hard") {
+  document.getElementById(localStorage.difficulty).checked = true;
+}
+function difficulty(d) {
+  if (d == "Easy") {
+    localStorage.difficulty = "Easy";
+    localStorage.time = "2000";
+  } else if (d == "Medium") {
+    localStorage.difficulty = "Medium";
+    localStorage.time = "1500";
+  } else if (d == "Hard") {
+    localStorage.difficulty = "Hard";
+    localStorage.time = "1000";
+  }
+}
